d6c01a5ee7836b65a477d1689c07bc3c34d15b77
Juliana Fogg <julianafogg@gmail.com>
2017-09-21 00:29:29 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+
+  attr_reader :roster #
+
+  def initialize (roster) #
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    self.roster[grade] ||= [] 
+    #if self.roster already exist leave it alone or else set self.roster[grade] = []
+    self.roster[grade] << name 
+    #   if @roster.has_key?(grade)
+    #    @roster[grade] << name
+    #   else
+    #    @roster[grade] = [name]
+    # end
+ end
+
+ def grade(grade)
+   self.roster[grade]x
+ end
+end
